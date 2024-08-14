859e9b2f517b2d115f0881300c1074ba718bb8c5
Helen <hongphuc292000@gmail.com>
2024-08-14 16:50:58 +0700

    feat(14-08-2024): Update eslint and prettier config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { IConfiguration } from './config/configuration.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get<IConfiguration>(ConfigService);
+  await app.listen(configService.port);
 }
 bootstrap();
